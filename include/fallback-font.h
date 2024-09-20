8444622c56fc34e7a8fc06340c12085560a1585a
Abdalrzag Eisa <bmjfdrh@gmail.com>
2024-09-20 11:12:37 +0300

    add fallback font

---
+++ include/fallback-font.h
@@ -0,0 +1,3 @@
+const char FALLBACK_FONT[] = "iawodijaoijd1\n"
+                             "awdawdawd2\n"
+                             "awdawodjoaijdiojawd3";
--- main.c
+++ main.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main()
+#include "include/fallback-font.h"
+
+/// font_text: The font file as text
+void parse_font(char *font_text)
 {
-    printf("hello\n");
+}
+
+int main(int argc, char **argv)
+{
+    if (argc <= 1)
+    {
+        printf("Missing message!\n");
+        exit(1);
+    }
+
+    char *message = malloc(128 * sizeof(char));
+
+    // Parse args and extract the message
+    for (int i = 1; i < argc; ++i)
+    {
+        strncpy(message, argv[i], strlen(argv[i]) + 1);
+    }
+
+    printf("%s\n", FALLBACK_FONT);
 
     return 0;
 }
